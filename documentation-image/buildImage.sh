2215728db13f292340cff5a573f4ba482ccc9d95
root <patrickalin@gmail.com>
2018-03-17 21:49:27 +0100

    new version

--- documentation-image/buildImage.sh
+++ documentation-image/buildImage.sh
@@ -5,11 +5,6 @@ cd $DIRECTORY
 set -e
 source ./env
 
-RED='\e[1;31m'
-BLUE='\e[1;34m'
-GREEN='\e[1;32m'
-NC='\e[0m'
-
 echo "##################################"
 echo -e "$BLUE Test internet / proxy $NC"
 
@@ -24,7 +19,7 @@ if [ $status != 200 ]; then echo $status; echo -e "\e[1;31m $REGISTRY down \e[0m
 echo -e "\e[1;32m $status \e[0m"
 
 echo "\n##################################"
-echo -e "$BLUE Image to pull : $GREEN" $REGISTRY/$IMAGE_SRC:$TAG_SRC "$NC"
+echo -e "$BLUE Image to pull : $GREEN" $IMAGE_SRC:$TAG_SRC "$NC"
 docker pull $IMAGE_SRC:$TAG_SRC
 
 echo "##################################"
@@ -38,14 +33,22 @@ docker push $REGISTRY/$IMAGE_SRC:$TAG_SRC
 echo "##################################"
 echo -e "$BLUE Remove : $GREEN" $IMAGE_SRC:$TAG_SRC "$NC"
 docker rmi $IMAGE_SRC:$TAG_SRC
+
+echo "##################################"
+echo -e "$BLUE Generate Dockerfile $NC"
+echo "# Don't edit this file, user DockerfileTemplate and buildImage.sh" > Dockerfile
+echo "###############################################" >> Dockerfile
+$(REGISTRY=$REGISTRY IMAGE_SRC=$IMAGE_SRC TAG_SRC=$TAG_SRC envsubst < DockerfileTemplate >> Dockerfile)
+
+echo "##################################"
+echo -e "$BLUE Build and push $GREEN $REGISTRY/$IMAGE_DEST-$POSTFIX:$TAG_DEST $NC"
+docker build -t $REGISTRY/$IMAGE_DEST-$POSTFIX:$TAG_DEST .
+docker push $REGISTRY/$IMAGE_DEST-$POSTFIX:$TAG_DEST
+
+echo "##################################"
+echo -e "$BLUE Remove : $GREEN" $IMAGE_SRC:$TAG_SRC "$NC"
 docker rmi $REGISTRY/$IMAGE_SRC:$TAG_SRC
 
 echo "##################################"
-echo -e "$BLUE Build and push $GREEN $REGISTRY/$IMAGE_DEST:$TAG_DEST $NC"
-docker build -t $REGISTRY/$IMAGE_DEST:$TAG_DEST .
-docker push $REGISTRY/$IMAGE_DEST:$TAG_DEST
-
-echo "##################################"
 echo -e "$BLUE List images $NC"
-docker images | grep $IMAGE_SRC
 docker images | grep $IMAGE_DEST
