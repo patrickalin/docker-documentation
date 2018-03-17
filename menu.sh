ddc7939aaec8e3f02fb3f6e91fe6719441b997d1
root <patrickalin@gmail.com>
2018-03-17 21:50:19 +0100

    new version

--- menu.sh
+++ menu.sh
@@ -5,12 +5,16 @@ cd $DIRECTORY
 
 set -e
 
+
 SERVICE="$(basename `pwd` | cut -d'-' -f 2)"
 IMAGE="$SERVICE-image"
 
+source ./$IMAGE/env
+FROM="($IMAGE_SRC : $TAG_SRC)"
+
 if [ -d "$IMAGE" ]; then
 OPTION=$(whiptail --title $SERVICE --menu "Choose your option" 15 60 4 \
-"1" "Build $SERVICE" \
+"1" "Build from $FROM" \
 "2" "(Re)Start service $SERVICE" \
 "3" "Stop service $SERVICE" 3>&1 1>&2 2>&3)
 else
@@ -35,7 +39,7 @@ case "$OPTION" in
 2)  docker stack remove  $SERVICE
     sleep 3
     docker stack deploy --compose-file docker-compose.yml $SERVICE
-    if [ -d "$IMAGE" ]; then 
+    if [ -d "$IMAGE" ]; then
     echo "######################"
     source $IMAGE/env
     fi
